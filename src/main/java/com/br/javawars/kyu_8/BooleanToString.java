89b1cb4bd5150d726ab64ab81c3dc4a2fdcef4a2
CleuJunior <cleu.junior@gmail.com>
2022-03-14 02:50:08 -0300

    complete: Conversion of Boolean to String

---
+++ src/main/java/com/br/javawars/kyu_8/BooleanToString.java
@@ -0,0 +1,6 @@
+package com.br.javawars.kyu_8;
+
+public class BooleanToString {
+
+    public static String convert(boolean b){ return b ? "true" : "false"; }
+}
---
+++ src/test/java/com/br/javawars/kyu_8/BooleanToStringTest.java
@@ -0,0 +1,15 @@
+package com.br.javawars.kyu_8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BooleanToStringTest {
+
+    @Test
+
+    public void testTrue(){ assertEquals(BooleanToString.convert(true), "true"); }
+
+    @Test public void testFalse(){ assertEquals(BooleanToString.convert(false), "false"); }
+
+}
